f869a2cd01d56dc564cf537a6d02f70f1e94b0c3
Jens Reidel <jens@troet.org>
2020-09-23 18:27:49 +0200

    Remove unneeded uses

--- redis/src/lib.rs
+++ redis/src/lib.rs
@@ -33,9 +33,9 @@ use rarity_cache::{
 };
 use serde::{de::DeserializeOwned, Serialize};
 #[cfg(not(feature = "simd-json"))]
-pub use serde_json::{from_slice, from_str, to_string, to_vec, Error as JsonError};
+pub use serde_json::{from_slice, to_vec};
 #[cfg(feature = "simd-json")]
-pub use simd_json::{from_slice, from_str, to_string, to_vec, Error as JsonError};
+pub use simd_json::{from_slice, to_vec};
 use std::marker::PhantomData;
 use twilight_model::id::{AttachmentId, ChannelId, EmojiId, GuildId, MessageId, RoleId, UserId};
 
